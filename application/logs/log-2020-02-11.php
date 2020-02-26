c19c8d5b51b8320215a971a4165d81145934a3a5
Indrajit <you@example.com>
2020-02-26 12:19:02 +0530

    Dateout Project

---
+++ application/logs/log-2020-02-11.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-02-11 07:26:30 --> 404 Page Not Found: /index
+ERROR - 2020-02-11 07:27:01 --> 404 Page Not Found: /index
---
+++ application/logs/log-2020-02-26.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-02-26 07:19:46 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:19:46 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:51 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:51 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:51 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:51 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:51 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:51 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:35:52 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:37:02 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:37:57 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:42:33 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:43:00 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:46:17 --> 404 Page Not Found: /index
+ERROR - 2020-02-26 07:46:23 --> 404 Page Not Found: /index
