36fee63685a817837fd901172143219027be2e68
Your Name <indrajit.auddy27@gmail.com>
2020-03-13 13:11:48 +0530

    changes

---
+++ application/logs/log-2020-02-28.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-02-28 14:39:36 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'ci_rest_api' C:\xampp\htdocs\dateout\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-02-28 14:39:36 --> Unable to connect to the database
+ERROR - 2020-02-28 14:40:42 --> Query error: Table 'dateout.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-02-28 14:41:25 --> Query error: Table 'dateout.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-02-28 14:42:17 --> Query error: Table 'dateout.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-02-28 14:42:19 --> Query error: Table 'dateout.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-02-28 14:48:02 --> 404 Page Not Found: /index
+ERROR - 2020-02-28 14:48:03 --> 404 Page Not Found: /index
+ERROR - 2020-02-28 14:48:03 --> 404 Page Not Found: /index
+ERROR - 2020-02-28 14:48:03 --> 404 Page Not Found: /index
+ERROR - 2020-02-28 14:48:05 --> 404 Page Not Found: /index
+ERROR - 2020-02-28 16:07:38 --> Image Upload Error: <p>You did not select a file to upload.</p>
